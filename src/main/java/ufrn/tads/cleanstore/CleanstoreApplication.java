f7d59ae6d9e180c34a89e920727cf54242eefd13
Erick Medeiros <erick_mmedeiros@hotmail.com>
2021-08-09 13:26:27 -0300

    initial commit

---
+++ src/main/java/ufrn/tads/cleanstore/CleanstoreApplication.java
@@ -0,0 +1,13 @@
+package ufrn.tads.cleanstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufrn/tads/cleanstore/CleanstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ufrn.tads.cleanstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
